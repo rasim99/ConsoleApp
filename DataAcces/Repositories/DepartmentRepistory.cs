6d4a4c087903b7c11c752a5d3b473b5b3ca772aa
rasim99 <rasimjm@code.edu.az>
2023-01-02 01:25:17 +0400

    AddRepository-DepartmentRepository

---
+++ DataAcces/Repositories/DepartmentRepistory.cs
@@ -0,0 +1,89 @@
+﻿using DataAcces.Interfaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Repositories
+{
+    public class DepartmentRepistory : IRepistory<Department>
+    {
+        public bool Create(Department entity)
+        {
+            try
+            {
+                AppDbContext.Departments.Add(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public bool Delete(Department entity)
+        {
+            try
+            {
+                AppDbContext.Departments.Remove(entity);
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        public Department Get(Predicate<Department> filters = null)
+        {
+            try
+            {
+                return filters != null ? AppDbContext.Departments.Find(filters) : AppDbContext.Departments[0];
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public List<Department> GetAll(Predicate<Department> filters = null)
+        {
+            try
+            {
+                return filters == null ? AppDbContext.Departments : AppDbContext.Departments.FindAll(filters);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+
+        }
+
+        public bool Update(Department entity)
+        {
+            try
+            {
+                Department exisDepartment = Get(d => d.Id == entity.Id);
+                if (exisDepartment != null)
+                {
+                    exisDepartment = entity;
+                    return true;
+                }
+                return false;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ DataAcces/Repositories/EmployeeRepistory.cs
@@ -0,0 +1,38 @@
+﻿using DataAcces.Interfaces;
+using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcces.Repositories
+{
+    public class EmployeeRepistory : IRepistory<Employee>
+    {
+        public bool Create(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee Get(Predicate<Employee> filters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetAll(Predicate<Employee> filters = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Employee entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
