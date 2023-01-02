d38640c618c8d52898923982a94489f582483c6b
rasim99 <rasimjm@code.edu.az>
2023-01-02 22:02:43 +0400

    add IDepartment / Interfaces/Buisness

---
+++ Business/Interfaces/IDepartment.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public  interface IDepartment
+    {
+        Department Create (Department department);
+         Department Update (int id , Department department);
+        Department Delete (int id);
+        Department Get (int id);
+        Department Delete (string  name);
+        List<Department> GetAll (string name);
+        List<Department> GetAll ();
+    }
+}
