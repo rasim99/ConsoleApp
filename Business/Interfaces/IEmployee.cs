070da4306f1babf06ea0fbbb19981910df29d8c4
rasim99 <rasimjm@code.edu.az>
2023-01-02 22:11:31 +0400

    add IEmployee/Interfaces/Buisness

---
+++ Business/Interfaces/IEmployee.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    public interface IEmployee
+    {
+        Employee Create(Employee employee);
+         Employee Update(int id ,Employee employee);
+        Employee Delete(int id);
+        Employee Get(int id);
+        List<Employee> GetAll(string name);
+        List<Employee> GetAll();
+        
+    }
+}
