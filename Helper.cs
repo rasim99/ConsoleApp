bd048e46f71a721ed33e22b987192228771b4836
rasim99 <115929535+rasim99@users.noreply.github.com>
2023-01-07 00:44:06 +0400

    added Helpers

---
+++ Helper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.Helpers
+{
+    public class Helper
+    {
+        public static void HelperMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        public enum Buttons
+        {
+             CreateDepartment=1,
+            UpdateDepartment,
+            DeleteByIdDepartment,
+            GetByIdDepartment,
+            DeleteByNameDepartment,
+            GetallByCapasityDepartment,
+            GetAllDepartment,
+            CreateEmployee,
+            UpdateEmployee,
+            DeleteEmployeeById,
+            Searchbydepartmen,
+            GetByIdEmployee,
+            GetAllEmployee,
+            GetAllByName,
+            ExitProgram
+        };
+
+
+            //Console.WriteLine(" WellCome!");
+            //Console.WriteLine(" Choose one");
+            //" 1- CreateDepartment" +
+            //"2-UpdateDepartment" +
+            //"3-DeleteByIdDepartment" +
+            //"4-GetByIdDepartment" +
+            //"5- DeleteByNameDepartment" +
+            //"6-GetallByCapasityDepartment" +
+            //"7-GetAllDepartment" +
+            // 8-CreateEmployee
+
+            //"9-ExitProgram
+
+        
+    }
+}
